3f175f9a70a25351428fe31361aea880e8d3b064
khalifa ababacar diadhiou <diadhiouababacarpro@gmail.com>
2023-06-23 05:15:52 -0700

    projet reclamation client

---
+++ src/main/java/sn/finnapps/ReclamationRecord/ReclamationRecordApplication.java
@@ -0,0 +1,13 @@
+package sn.finnapps.ReclamationRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReclamationRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReclamationRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/finnapps/ReclamationRecord/ReclamationRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.finnapps.ReclamationRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReclamationRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
